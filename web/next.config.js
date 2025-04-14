5ae6c5b622afcea9f17337a43dd1e6c33984f344
Travis <travisgerrard@gmail.com>
2025-04-13 21:28:09 -0700

    fix: update Next.js config with explicit path aliases

--- web/next.config.js
+++ web/next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@supabase/supabase-js'],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
